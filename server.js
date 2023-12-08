71683bfd829c97134e623b451bde7e5530323cf8
gambo2007 <gambo7245@gmail.com>
2023-12-08 11:52:28 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:7456'); // Thay thế URL của máy chủ Cocos Creator của bạn
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    const socketId = socket.id;
+
+    socket.on('gameData', (userData) => {
+        console.log(JSON.stringify(userData) +`${socketId} connected`);
+    });
+
+    socket.on('messageData', (data) => {
+        // Xử lý dữ liệu nhận được từ sự kiện 'messageData'
+        console.log(`Received message data from ${socket.id}:`, data.message);
+
+        // Broadcast tin nhắn đến tất cả các client khác
+        io.emit('messageData', {
+            username: data.nameUser,
+            message: data.message,
+            socketID: data.socketID,
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
